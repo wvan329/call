ac1072a19eb10b33fbaf9e262ea25a768f5b00c6
wgk <2287334379@qq.com>
2025-06-10 14:15:17 +0800

    yes

--- src/main/java/cn/websocket/config/SignalingHandler.java
+++ src/main/java/cn/websocket/config/SignalingHandler.java
@@ -1,5 +1,6 @@
 package cn.websocket.config;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,6 +9,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Component
 public class SignalingHandler extends TextWebSocketHandler {
 
     private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
--- src/main/java/cn/websocket/config/WebSocketConfig.java
+++ src/main/java/cn/websocket/config/WebSocketConfig.java
@@ -1,17 +1,22 @@
 package cn.websocket.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import java.util.logging.SocketHandler;
+
 @Configuration
-@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private SignalingHandler handler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SignalingHandler(), "/ws");
-
-//                .setAllowedOrigins("*");
+        registry.addHandler(handler, "/ws")
+                .setAllowedOrigins("*");
     }
 }
