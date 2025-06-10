66fdab011be828b55c33975d26d7a13431138be0
wgk <2287334379@qq.com>
2025-06-10 11:57:37 +0800

    yes

--- src/main/java/cn/websocket/config/WebSocketConfig.java
+++ src/main/java/cn/websocket/config/WebSocketConfig.java
@@ -11,6 +11,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new SignalingHandler(), "/ws");
+
 //                .setAllowedOrigins("*");
     }
 }
